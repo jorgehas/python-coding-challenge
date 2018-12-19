523a9d05cfa7404f241d0e4684e6f66e33844ba8
jorgehas <hamjorg@yahoo.fr>
2018-12-19 14:48:45 -0500

    defined the body of the function compareVersions

--- Question_B/compare_version.py
+++ Question_B/compare_version.py
@@ -16,11 +16,45 @@ Output:
 
 '''
 
-def compareVersions(line1, line2):
+def compareVersions(version1, version2):
 
 	'''
-	
+	The function takes as two inputs version1 and version2
+	and return the version1 greater than version2 or
+	version1 less than version2 or version1 and version2 are equal
 	'''
+	#split by the character "."
+	sp_version1 = version1.split(".")
+	sp_version2 = version2.split(".")
+	# Get the length of the string
+	l1 = len(sp_version1)
+	l2 = len(sp_version2)
+	if l1 > l2:
+		sp_version1 += ['0' for _ in range(l1 - l2 )]
+	elif(l1 < l2):
+		sp_version2 += ['0' for _ in range(l2 - l1 )]
+
+	index = 0
+	while index < l1:
+		if int(sp_version1[index]) > int(sp_version2[index]):
+			return "version 1 is greater than version 2"
+		elif int(sp_version1[index]) < int(sp_version2[index]):
+			return "version 1 is less than version 2"
+		else:
+			index +=1
+	
+	return "version 1 and version 2 are equal"
+	
+#Test
+if __name__ == '__main__':
+	print(compareVersions("7.3.2.1","7.2.8.1"))
+	print(compareVersions("24.0","114.1.0"))
+	print(compareVersions("05.0","5.0.0"))
+
+
+
 	
 
+
+ 
 
