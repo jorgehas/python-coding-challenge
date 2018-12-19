cd57f657efcd94e89e9f640ea2a7eda4b30544f8
jorgehas <hamjorg@yahoo.fr>
2018-12-19 13:53:17 -0500

    Created a file for Question B

---
+++ Question_B/compare_version.py
@@ -0,0 +1,26 @@
+'''
+Author: Georges Hassana
+12/19/2018
+
+Description:
+	The goal of this question is to write a software library that 
+	accepts 2 version string as input and returns whether one is 
+	greater than, equal, or less than the other. 
+
+Example: 
+“1.2” is greater than “1.1”.
+
+Output:
+“1.2” is greater than “1.1”.
+
+
+'''
+
+def compareVersions(line1, line2):
+
+	'''
+	
+	'''
+	
+
+
