382275ccb4f2e58b8fc43870ae766d6af4cb71a9
jorgehas <hamjorg@yahoo.fr>
2018-12-19 16:32:58 -0500

    updated

--- Question_C/LRUCache.py
+++ Question_C/LRUCache.py
@@ -18,7 +18,7 @@ Example:
 '''
 import time
 
-#Define a class Node that with enable access to the cache contents
+#Define a class Node that will enable access to the cache contents
 class Node(object):
 
 	def __init__(self,key, value, time):
@@ -27,7 +27,7 @@ class Node(object):
 		self.time = time
 		self.previous = None
 		self.next = None
-
+#Define a class LinkedList
 class LinkedList(object):
 	def __init__(self):
 		self.head = None
@@ -57,29 +57,71 @@ class LinkedList(object):
 
 class LRUCache:
 	def __init__(self, size):
-		cur_time = time.time()
 		self.list = LinkedList()
 		self.size = size
-		self.dict = {}
+		self.cache = {}
+		cur_time = time.time()
 
-	def _add(self,key,value):
-		node = Node(key,value)
+		#expiry time
+		self.expiry_time = 30
+
+	def __setitem__(self, key, value):
+        # .
+        if key in self.cache:
+            node = self.cache[key]
+
+            # Replace the value.
+            node.value = value
+
+            # Update the list ordering.
+            self.mtf(node)
+            self.head = node
+
+	def __additem__(self,key,value):
+		node = Node(key, value, cur_time)
 		self.list.__insertitem__(node)
-		self.dict[key] = node
+		self.cache[key] = node
+		cur_time = time.time()
 
-	def put():
-		if key in self.dict:
-			self.list.__removeitem__(self.dict[key])
 
-		elif len(self.dict) == self.size:
-			
+	def put(self,key,value):
+		
+		if key in self.cache:
+			self.list.__removeitem__(self.cache[key])
 
+		elif len(self.cache) == self.size:
+			del self.cache[self.list.head.key]
+			self.list.__removeitem__(self.list.head)
+
+		self._additem__(key,value)
+
+	def get(self,key,value):
+
+		cur_time = time.time()
+		if key in self.cache:
+			node = self.cache[key]
+			self.cache[key].time = cur_time
+
+		self.__removeitem__(node)
+		sel.__insertitem__(node)
+		return node.value
+
+	def _remove_expiredNode():
+		
+		if key in self.cache:
+			node = self.cache[key]
+			if cur_time - node.time > self.expiry_time:
+				self.__removeitem__(node)
+				del self.cache[node.key]
+
+
+
+if __name__ == "__main__":
+    cache = LRUCache(3,3,30)
+    cache.__setitem__(1, 1, 10)
+    cache.__setitem__(2, 2, 9)
+    print(cache.get(3,3,20))
 	
-
-
-
-
-
 
 
 
